f24f5556087959fce215db922e8c3820c23f4594
Ji <hwichance@gmail.com>
2022-07-11 21:37:59 +0900

    [CHORE] #3 - create package
    - craete package associated with networking
    - create ui package and move activities

--- app/src/main/java/com/example/android_repo_05/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/android_repo_05/ui/activities/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android_repo_05
+package com.example.android_repo_05.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_repo_05.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
