a527845c50d7bd7b3ffe8f6405edb08cfeff393e
woowahan <nosod3@naver.com>
2022-07-13 02:38:00 +0900

    [FEAT] #2 - MainActivity 재생성시 보고 있던 프래그먼트 탭 유지를 위한 viewModel 작성

---
+++ app/src/main/java/com/example/android_repo_05/viewmodel/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.android_repo_05.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android_repo_05.MainTabType
+import com.example.android_repo_05.R
+
+class MainViewModel : ViewModel() {
+    private val _currentTabFragment = MutableLiveData(MainTabType.ISSUE)
+    val currentTabFragment : LiveData<MainTabType> = _currentTabFragment
+
+    private fun getMainTabType(itemId : Int) : MainTabType =
+        when(itemId) {
+            R.id.tabButton_issue -> MainTabType.ISSUE
+            R.id.tabButton_notification -> MainTabType.NOTIFICATION
+            else -> throw IllegalArgumentException()
+        }
+
+    fun setCurrentTab(itemId : Int) : Boolean {
+        changeCurrentTab(getMainTabType(itemId))
+        return true
+    }
+
+    private fun changeCurrentTab(mainTabType : MainTabType) {
+        if (currentTabFragment.value != mainTabType) _currentTabFragment.value = mainTabType
+    }
+
+}
