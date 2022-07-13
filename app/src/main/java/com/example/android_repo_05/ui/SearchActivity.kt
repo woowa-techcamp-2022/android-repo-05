d7f5eea6d62e6f745d010b2f008d70ad3ff0eac0
woowahan <nosod3@naver.com>
2022-07-13 11:55:49 +0900

    [ADD] #2 - ProfileActivity, SearchActivity 파일 추가

---
+++ app/src/main/java/com/example/android_repo_05/ui/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_repo_05.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_repo_05.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
