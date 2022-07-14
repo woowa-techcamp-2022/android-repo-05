27fb85410d33db365b7001a7be1ab238f76519d5
woowahan <nosod3@naver.com>
2022-07-15 00:02:28 +0900

    [ADD] #15 - AuthInterceptor 추가 및 적용

---
+++ app/src/main/java/com/example/android_repo_05/retrofit/AuthInterceptor.kt
@@ -0,0 +1,16 @@
+package com.example.android_repo_05.retrofit
+
+import com.example.android_repo_05.base.CustomApplication.Companion.tokenRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = runBlocking { tokenRepository.accessTokenFlow.first() }
+        val req = chain.request().newBuilder().addHeader("Accept", "application/vnd.github+json")
+            .addHeader("Authorization", "token $accessToken").build()
+        return chain.proceed(req)
+    }
+}
