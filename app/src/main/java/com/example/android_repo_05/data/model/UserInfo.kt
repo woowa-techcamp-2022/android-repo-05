dc2aade381f18df7b518392bf866a196add42776
Ji <hwichance@gmail.com>
2022-07-14 16:53:34 +0900

    [FEAT] #18 - 유저 정보를 담을 model 생성
    - response schema에 따라 타입과 nullable 설정

---
+++ app/src/main/java/com/example/android_repo_05/data/model/UserInfo.kt
@@ -0,0 +1,28 @@
+package com.example.android_repo_05.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UserInfo(
+    @SerializedName("avatar_url")
+    val profileImageUrl: String,
+    @SerializedName("name")
+    val displayName: String?,
+    @SerializedName("login")
+    val userName: String,
+    @SerializedName("bio")
+    val bio: String?,
+    @SerializedName("location")
+    val location: String?,
+    @SerializedName("blog")
+    val link: String?,
+    @SerializedName("email")
+    val email: String?,
+    @SerializedName("followers")
+    val followersCount: Int,
+    @SerializedName("following")
+    val followingCount: Int,
+    @SerializedName("public_repos")
+    val publicReposCount: Int,
+    @SerializedName("total_private_repos")
+    val privateReposCount: Int
+)
