dfd36424d712cca0926a4555d89abe0e5f7e4561
Ji <hwichance@gmail.com>
2022-07-15 10:37:05 +0900

    [FEAT] #21 - 프로필 이미지 로드 구현
    - Glide와 bindingAdapter로 프로필 이미지 로드

--- app/src/main/java/com/example/android_repo_05/adapters/AppBindingAdapter.kt
+++ app/src/main/java/com/example/android_repo_05/adapters/AppBindingAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.android_repo_05.adapters
 
-class AppBindingAdapter {
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imgUrl")
+fun loadImageFromUrl(view: ImageView, url: String?) {
+    if(!url.isNullOrBlank()) {
+        Glide.with(view.context)
+            .load(url)
+            .into(view)
+    }
 }
