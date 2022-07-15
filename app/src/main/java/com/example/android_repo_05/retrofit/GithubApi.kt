05e5513265dab7abd99500fd0289ccfcc2e359b9
woowahan <nosod3@naver.com>
2022-07-16 00:14:28 +0900

    [FEAT] #17 - Issues GET 추가

--- app/src/main/java/com/example/android_repo_05/retrofit/GithubApi.kt
+++ app/src/main/java/com/example/android_repo_05/retrofit/GithubApi.kt
@@ -1,6 +1,7 @@
 package com.example.android_repo_05.retrofit
 
 import com.example.android_repo_05.BuildConfig
+import com.example.android_repo_05.data.model.IssueResponse
 import com.example.android_repo_05.data.model.LoginResponse
 import com.example.android_repo_05.data.model.UserInfo
 import com.example.android_repo_05.data.model.UserProfileResponse
@@ -21,8 +22,12 @@ interface GithubApi {
     @GET("user")
     suspend fun getUserInfo(): Response<UserInfo>
 
-    @GET("/issues")
-    suspend fun getIssues(): Response<UserInfo>
+    @GET("/user/issues")
+    suspend fun getIssues(
+        @Query("page") page : Int,
+        @Query("num") num : Int = 10,
+        @Query("state") state : String = "all"
+    ): ArrayList<IssueResponse>
 
     @GET("/user")
     suspend fun getProfileUrl() : Response<UserProfileResponse>
