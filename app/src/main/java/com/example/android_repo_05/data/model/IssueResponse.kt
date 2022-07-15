56c197abbba6355aaf7a2f07c2ce5853f066ba66
woowahan <nosod3@naver.com>
2022-07-16 00:08:35 +0900

    [ADD] #17 - IssueResponse 추가

---
+++ app/src/main/java/com/example/android_repo_05/data/model/IssueResponse.kt
@@ -0,0 +1,21 @@
+package com.example.android_repo_05.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class IssueResponse(
+    @SerializedName("id")
+    val id : Int,
+    @SerializedName("state")
+    val state : String,
+    @SerializedName("title")
+    val title : String,
+    @SerializedName("updated_at")
+    val updated_at : String,
+    @SerializedName("repository")
+    val repository: Repository
+)
+
+data class Repository(
+    @SerializedName("full_name")
+    val full_name: String
+)
