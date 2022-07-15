0a67685df9f6a99f9810ae40eb49082b1709bbca
woowahan <nosod3@naver.com>
2022-07-15 11:06:41 +0900

    [REFACTOR] #22 - DataStore에서 AccessToken 접근하는 함수 삭제

--- app/src/main/java/com/example/android_repo_05/repositories/GithubApiRepository.kt
+++ app/src/main/java/com/example/android_repo_05/repositories/GithubApiRepository.kt
@@ -1,22 +1,14 @@
 package com.example.android_repo_05.repositories
 
-import android.content.Context
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.stringPreferencesKey
-import com.example.android_repo_05.data.datastore.dataStore
 import com.example.android_repo_05.retrofit.GithubApiInstance
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import java.io.IOException
 
 class GithubApiRepository {
     companion object {
-        private var repo: GithubApiRepository? = null
+        var githubApiRepo: GithubApiRepository? = null
 
         fun getInstance(): GithubApiRepository {
-            return repo ?: synchronized(this) {
-                repo ?: GithubApiRepository().also { repo = it }
+            return githubApiRepo ?: synchronized(this) {
+                githubApiRepo ?: GithubApiRepository().also { githubApiRepo = it }
             }
         }
     }
@@ -24,22 +16,6 @@ class GithubApiRepository {
     suspend fun getAccessTokenFromRemote(code: String) =
         GithubApiInstance.retrofit.getAccessToken(accessCode = code)
 
-    suspend fun getAccessTokenFromDataStore(context: Context) = context.dataStore.data
-        .catch { exception ->
-            if (exception is IOException) {
-                emit(emptyPreferences())
-            } else {
-                throw exception
-            }
-        }.map { preferences ->
-            preferences[stringPreferencesKey("accessToken")] ?: ""
-        }
-
-    suspend fun setAccessTokenToDataStore(context: Context, accessToken: String) =
-        context.dataStore.edit { pref ->
-            pref[stringPreferencesKey("accessToken")] = accessToken
-        }
-
-    suspend fun getUserInfoFromRemote(token: String) =
-        GithubApiInstance.retrofit.getUserInfo("token $token")
+    suspend fun getUserInfoFromRemote() =
+        GithubApiInstance.retrofit.getUserInfo()
 }
