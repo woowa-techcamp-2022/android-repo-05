b5f7b201693d8b54ce241c47f0e0e3f6e6688453
woowahan <nosod3@naver.com>
2022-07-15 11:06:05 +0900

    [REFACTOR] #22 - AppViewModelFactory 여러 Repository 대응되도록 수정

--- app/src/main/java/com/example/android_repo_05/viewmodels/AppViewModelFactory.kt
+++ app/src/main/java/com/example/android_repo_05/viewmodels/AppViewModelFactory.kt
@@ -3,17 +3,22 @@ package com.example.android_repo_05.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android_repo_05.repositories.GithubApiRepository
+import com.example.android_repo_05.repositories.ProfileImageRepository
 
 class AppViewModelFactory(
-    private val githubApiRepository: GithubApiRepository
+    private val githubApiRepository: GithubApiRepository? = GithubApiRepository.githubApiRepo,
+    private val profileImageRepository: ProfileImageRepository? = ProfileImageRepository.profileRepo
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
-                LoginViewModel(githubApiRepository) as T
+                LoginViewModel(githubApiRepository!!) as T
             }
             modelClass.isAssignableFrom(UserInfoViewModel::class.java) -> {
-                UserInfoViewModel(githubApiRepository) as T
+                UserInfoViewModel(githubApiRepository!!) as T
+            }
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(profileImageRepository!!) as T
             }
             else -> {
                 super.create(modelClass)
