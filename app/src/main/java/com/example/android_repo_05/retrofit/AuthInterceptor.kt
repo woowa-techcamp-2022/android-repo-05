af02663e27ddc394e0ad5531f4b79ed72c1d07e2
woowahan <nosod3@naver.com>
2022-07-15 18:42:14 +0900

    [REFACTOR] #22 - DataStore의 accessToken 호출 부분 수정

--- app/src/main/java/com/example/android_repo_05/retrofit/AuthInterceptor.kt
+++ app/src/main/java/com/example/android_repo_05/retrofit/AuthInterceptor.kt
@@ -1,6 +1,7 @@
 package com.example.android_repo_05.retrofit
 
-import com.example.android_repo_05.base.CustomApplication.Companion.tokenRepository
+import com.example.android_repo_05.base.CustomApplication.Companion.accessTokenFlow
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
@@ -8,7 +9,7 @@ import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val accessToken = runBlocking { tokenRepository.accessTokenFlow.first() }
+        val accessToken = runBlocking(Dispatchers.IO) { accessTokenFlow.first() }
         val req = chain.request().newBuilder().addHeader("Accept", "application/vnd.github+json")
             .addHeader("Authorization", "token $accessToken").build()
         return chain.proceed(req)
