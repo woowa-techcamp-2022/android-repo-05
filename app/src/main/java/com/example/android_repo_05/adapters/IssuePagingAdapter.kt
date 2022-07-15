9a42150d5bb158cb0f47f4aa8e6016426afeffde
woowahan <nosod3@naver.com>
2022-07-16 00:10:22 +0900

    [FEAT] #17 - IssuePagingAdapter 작성

---
+++ app/src/main/java/com/example/android_repo_05/adapters/IssuePagingAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.android_repo_05.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_repo_05.data.model.IssueResponse
+import com.example.android_repo_05.databinding.ItemIssueBinding
+
+class IssuePagingAdapter :
+    PagingDataAdapter<IssueResponse, IssuePagingAdapter.IssueViewHolder>(diffCallback = differ){
+
+    companion object {
+        private val differ = object : DiffUtil.ItemCallback<IssueResponse>() {
+            override fun areItemsTheSame(oldItem: IssueResponse, newItem: IssueResponse): Boolean {
+                return (oldItem.id == newItem.id)
+            }
+
+            override fun areContentsTheSame(oldItem: IssueResponse, newItem: IssueResponse): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IssueViewHolder =
+        IssueViewHolder(
+            ItemIssueBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: IssueViewHolder, position: Int) {
+        holder.binding.issue = getItem(position)
+    }
+
+    class IssueViewHolder(val binding : ItemIssueBinding) : RecyclerView.ViewHolder(binding.root)
+}
