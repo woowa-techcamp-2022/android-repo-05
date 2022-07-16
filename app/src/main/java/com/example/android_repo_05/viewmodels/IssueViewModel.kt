67697b59ab6acfb3c02d3e9bffa05e59e6f9172a
woowahan <nosod3@naver.com>
2022-07-16 20:14:35 +0900

    [REFACTOR] #17 - ViewModel IssueFiltering 데이터형식 LiveData -> StateFlow로 변경

--- app/src/main/java/com/example/android_repo_05/viewmodels/IssueViewModel.kt
+++ app/src/main/java/com/example/android_repo_05/viewmodels/IssueViewModel.kt
@@ -1,16 +1,21 @@
 package com.example.android_repo_05.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.example.android_repo_05.repositories.IssueRepository
+import com.example.android_repo_05.ui.fragments.IssueFiltering
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class IssueViewModel(repository : IssueRepository) : ViewModel() {
     val issueList = repository.getStockDataByPaging().cachedIn(viewModelScope)
 
-    private var _issueFiltering : MutableLiveData<String> = MutableLiveData()
-    val issueFiltering : LiveData<String> = _issueFiltering
+    private val _issueFiltering = MutableStateFlow<IssueFiltering>(IssueFiltering.Open)
+    val issueFiltering : StateFlow<IssueFiltering> = _issueFiltering
+
+    fun setIssueFiltering(filtering : IssueFiltering) {
+        _issueFiltering.value = filtering
+    }
 
 }
