519ef0699f33c77da6b3ace0251cbb94d9b46f73
Ji <hwichance@gmail.com>
2022-07-18 12:48:43 +0900

    [FEAT] #46 - Notification 최근 업데이트 시간 형식 설정
    - issueRepository의 date 형식 변경 메소드를 Utils로 옮김
    - IssueReponse나 NotificationModel에서 get() 메소드로 변경된 형식 값 가져오도록 구현
    - Notification item xml에 적용

--- app/src/main/java/com/example/android_repo_05/data/models/notification/NotificationModel.kt
+++ app/src/main/java/com/example/android_repo_05/data/models/notification/NotificationModel.kt
@@ -1,5 +1,6 @@
 package com.example.android_repo_05.data.models.notification
 
+import com.example.android_repo_05.others.Utils
 import com.google.gson.annotations.SerializedName
 
 data class NotificationModel(
@@ -13,4 +14,6 @@ data class NotificationModel(
     val updatedAt: String,
     @SerializedName("unread")
     val unread: Boolean
-)
+) {
+    val elapsedTime: String get() = Utils.calculateElapsedTime(updatedAt)
+}
--- app/src/main/java/com/example/android_repo_05/others/Utils.kt
+++ app/src/main/java/com/example/android_repo_05/others/Utils.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.net.Uri
 import android.util.TypedValue
 import com.example.android_repo_05.BuildConfig
+import com.example.android_repo_05.data.models.IssueResponse
+import com.example.android_repo_05.others.enums.TimeUnit
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
 object Utils {
+    private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.KOREA)
+
     fun getGithubIdentityRequestUri(): Uri = Uri.Builder().scheme("https").authority("github.com")
         .appendPath("login")
         .appendPath("oauth")
@@ -17,17 +22,48 @@ object Utils {
         .build()
 
     fun dpToPx(context: Context, dp: Float): Float {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        )
     }
 
-    val curTime = Date().time
-    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.KOREA)
+    fun calculateElapsedTime(updatedAt: String): String {
+        try {
+            simpleDateFormat.parse(updatedAt)?.let { data ->
+                val curTime = Date().time
+                val updateTime = data.time
+                var diffTime = (curTime - updateTime) / 1000
+                when {
+                    diffTime < TimeUnit.Second.timeUnit -> {
+                        return "${diffTime}초전"
+                    }
+                    diffTime < TimeUnit.Minute.timeUnit -> {
+                        diffTime /= TimeUnit.Second.timeUnit
+                        return "${diffTime}분전"
+                    }
+                    diffTime < TimeUnit.Hour.timeUnit -> {
+                        diffTime /= TimeUnit.Minute.timeUnit
+                        return "${diffTime}시간전"
+                    }
+                    diffTime < TimeUnit.Day.timeUnit -> {
+                        diffTime /= TimeUnit.Hour.timeUnit
+                        return "${diffTime}일전"
+                    }
+                    diffTime < TimeUnit.MONTH.timeUnit -> {
+                        diffTime /= TimeUnit.Day.timeUnit
+                        return "${diffTime}개월전"
+                    }
+                    else -> {
+                        diffTime /= TimeUnit.MONTH.timeUnit
+                        return "${diffTime}년전"
+                    }
+                }
+            }
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
 }
-enum class TimeUnit(val timeUnit : Int) {
-    Second(60),
-    Minute(60 * Second.timeUnit),
-    Hour(24 * Minute.timeUnit),
-    Day(30 * Hour.timeUnit),
-    MONTH(12 * Day.timeUnit)
-}
-
