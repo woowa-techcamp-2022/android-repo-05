222657f6685f5d985dba8cf611deb17e92c277bf
woowahan <nosod3@naver.com>
2022-07-21 00:34:49 +0900

    [REFACTOR] #28 - MainActivity DataBinding 적용

--- app/src/main/java/com/example/android_repo_05/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/android_repo_05/ui/activities/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.example.android_repo_05.ui.activities
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
 import com.example.android_repo_05.R
 import com.example.android_repo_05.customviews.MainTabButton
 import com.example.android_repo_05.data.models.ResponseState
 import com.example.android_repo_05.databinding.ActivityMainBinding
-import com.example.android_repo_05.data.repositories.ProfileImageRepository
 import com.example.android_repo_05.ui.fragments.IssueFragment
 import com.example.android_repo_05.ui.fragments.NotificationFragment
 import com.example.android_repo_05.ui.viewmodels.AppViewModelFactory
@@ -32,18 +25,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        mainViewModel.getProfileUrlFromRemote()
+        getImageProfile()
         initViews()
         observe()
     }
 
     private fun initViews() {
-        // 메인 탭 버튼 그룹의 클릭리스너 등록
-        binding.tabBtnGroup.children.forEach { view ->
-            if (view is MainTabButton) {
-                view.setOnClickListener(this)
-            }
-        }
+        binding.lifecycleOwner = this
+        binding.mainActivity = this
 
         binding.tbMain.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
@@ -61,11 +50,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         mainViewModel.profileUrl.observe(this) { responseState ->
-            if(responseState is ResponseState.Success) {
-                drawProfileImage(responseState.data!!.profileImageUrl)
+            if (responseState is ResponseState.Success) {
+                binding.profileUrl = responseState.data!!.profileImageUrl
             }
         }
-
     }
 
     private fun checkButton(id: Int) {
@@ -105,20 +93,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         transaction.commitAllowingStateLoss()
     }
 
-    private fun drawProfileImage(url : String) {
-        Glide.with(this).asBitmap().load(url).placeholder(R.drawable.ic_user).circleCrop()
-            .into(object : CustomTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    binding.tbMain.menu.findItem(R.id.menu_profile).icon =
-                        BitmapDrawable(resources, resource)
-                }
-
-                override fun onLoadCleared(placeholder: Drawable?) {
-                    binding.tbMain.menu.findItem(R.id.menu_profile).icon = placeholder
-                }
-            })
+    private fun getImageProfile() {
+        mainViewModel.getProfileUrlFromRemote()
     }
-
 }
 
 enum class MainTabType(val tag: String, val id: Int) {
