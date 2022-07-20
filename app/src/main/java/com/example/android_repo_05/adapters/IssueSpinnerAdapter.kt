6438fdf80246aaef4aa5a9925b028f7c4eabb546
woowahan <nosod3@naver.com>
2022-07-20 19:02:49 +0900

    [FEAT] #28 - IssueSpinnerAdapter 데이터 바인딩 적용

--- app/src/main/java/com/example/android_repo_05/adapters/IssueSpinnerAdapter.kt
+++ app/src/main/java/com/example/android_repo_05/adapters/IssueSpinnerAdapter.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import androidx.core.content.ContextCompat
-import com.example.android_repo_05.R
 import com.example.android_repo_05.databinding.ItemIssueFilterBinding
 import com.example.android_repo_05.databinding.ItemIssueFilterHeadBinding
 import com.example.android_repo_05.ui.fragments.IssueFiltering
@@ -16,9 +14,15 @@ class IssueSpinnerAdapter(
 ) : BaseAdapter() {
 
     private var selectedFilter: IssueFiltering = IssueFiltering.Open
+    private var isSpinnerSelected : Boolean = false
 
     fun setSelectedFilter(selectedFilter: IssueFiltering) {
         this.selectedFilter = selectedFilter
+    }
+
+    fun setSpinnerSelected(isSpinnerSelected : Boolean) {
+        this.isSpinnerSelected = isSpinnerSelected
+        notifyDataSetChanged()
     }
 
     override fun getCount(): Int = IssueFiltering.values().size
@@ -27,24 +31,22 @@ class IssueSpinnerAdapter(
 
     override fun getItemId(position: Int): Long = 0
 
+    /*
+        데이터 바인딩 적용시 깜빡거림 현상이 있어 뷰바인딩 적용!
+     */
     override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
         val binding = ItemIssueFilterHeadBinding.inflate(LayoutInflater.from(context), viewGroup, false)
         binding.tvFilterHead.text = IssueFiltering.values()[position].filterName
+        binding.ivFilterHead.scaleY =
+            if (this.isSpinnerSelected) -1F
+            else 1F
         return binding.root
     }
 
     override fun getDropDownView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
         val binding = ItemIssueFilterBinding.inflate(LayoutInflater.from(context), viewGroup, false)
-        with(binding) {
-            tvFilterList.text = IssueFiltering.values()[position].filterName
-            if (selectedFilter == IssueFiltering.values()[position]) {
-                tvFilterList.setTextColor(ContextCompat.getColor(context, R.color.white))
-                ivFilterCheck.visibility = View.VISIBLE
-            } else {
-                tvFilterList.setTextColor(ContextCompat.getColor(context, R.color.grey))
-                ivFilterCheck.visibility = View.INVISIBLE
-            }
-        }
+        binding.issueFilter = IssueFiltering.values()[position]
+        binding.selectedFilter = this.selectedFilter
         return binding.root
     }
 
