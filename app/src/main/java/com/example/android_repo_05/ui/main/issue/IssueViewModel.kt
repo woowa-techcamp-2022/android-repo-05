b4d1ad43bb63dc191f3719492543911480edb00f
woowahan <nosod3@naver.com>
2022-07-24 18:27:42 +0900

    [REFACTOR] #35 - IssueRepository, IssueViewModel 종속 항목 삽입

--- app/src/main/java/com/example/android_repo_05/ui/main/issue/IssueViewModel.kt
+++ app/src/main/java/com/example/android_repo_05/ui/main/issue/IssueViewModel.kt
@@ -5,11 +5,16 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.filter
 import com.example.android_repo_05.data.main.issue.IssueRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
 
-class IssueViewModel(repository: IssueRepository) : ViewModel() {
+@HiltViewModel
+class IssueViewModel @Inject constructor(
+    repository: IssueRepository
+) : ViewModel() {
     private val issueList = repository.getStockDataByPaging().cachedIn(viewModelScope)
 
     private val _issueFiltering = MutableStateFlow(IssueFiltering.Open)
